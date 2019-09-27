59922fb4902fd0f2e08b531cd1a2b8fc40ff708a
Monica Barboza <barboza.monica2@gmail.com>
2019-09-27 18:58:58 -0500

    filtering working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,52 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import './App.style.scss'
-import HotelList from '../HotelList/HotelList'
+import React, { useState, useEffect } from "react";
+import "./App.style.scss";
+import HotelList from "../HotelList/HotelList";
 
-import hotelResultService from '../../services/hotel-result/hotel-result.service';
+import hotelResultService from "../../services/hotel-result/hotel-result.service";
 
 const App = () => {
-    const [hotels, setHotels] = useState([]);
-    const [filteredHotels, setFilteredHotels] = useState('')
-    const [selectedFilter, setSelectedFilter] = useState('')
-    const [searchHotel, setSearchHotel] = useState([])
+  const [hotels, setHotels] = useState([]);
+  const [filteredHotels, setFilteredHotels] = useState("");
+  const [selectedFilter, setSelectedFilter] = useState("");
+  const [searchHotel, setSearchHotel] = useState("");
 
-    useEffect(() => {
-        hotelResultService.get().then(response => {
-            setHotels(response.results.hotels)
-        })
-    }, []);
+  useEffect(() => {
+    hotelResultService.get().then(response => {
+      setHotels(response.results.hotels);
+    });
+  }, []);
 
-    const handleChange = event => {
+  const handleChange = event => {
+    console.log("in handle change");
+    setSearchHotel(event.target.value);
+  };
 
-        console.log("in handle select")
-        setSearchHotel(event.target.value)
-        console.log(searchHotel)
-    }
+  let hotelsToRender;
+  const hotelFiltering = () => {
+    //console.log("in filtering");
+    //hotelsToRender = hotels.filter(hotel => {
+    //   console.log("hotel:", hotel.hotelStaticContent.name.toLowerCase());
+    //   console.log("search content:", searchHotel);
+    //   console.log("includes?", hotel.hotelStaticContent.name
+    //   .toLowerCase()
+    //   .includes(searchHotel.toLowerCase()))
+    // console.log("hotels to render", hotelsToRender)
+    //   hotel.hotelStaticContent.name
+    //     .toLowerCase()
+    //     .includes(searchHotel.toLowerCase());
+        hotelsToRender = hotels.filter(hotel => (
+              hotel.hotelStaticContent.name
+                .toLowerCase()
+                .includes(searchHotel.toLowerCase())
+    ));
+  };
 
+  return (
+    //filter function here
+    //sort function here
 
-    return (
+    <div className="app-container">
+      <div className="content">
+        <div>
+          <div className="filters">
+            Hotel name
+            <input
+              type="text"
+              className="input"
+              maxLength={10}
+              value={searchHotel}
+              onChange={handleChange}
+              />
+            Price
+            <select name="" className="select">
+              <option value="">Recommended</option>
+              <option value="">Price low-to-high</option>
+              <option value="">Price high-to-low</option>
+            </select>
+            <button className="button">Reset</button>
+          </div>
+        </div>
 
+              {hotelFiltering()}
+        <HotelList hotels={hotelsToRender} />
 
-        <div className="app-container">
-            <div className="content">
-                <div>
-                    <div className="filters">
-                        Hotel name
-                        <input type="text" className="input" maxLength={10} value={searchHotel} onChange={handleChange}/>
-                        Price
-                        <select name="" className="select">
-                            <option value="">Recommended</option>
-                            <option value="">Price low-to-high</option>
-                            <option value="">Price high-to-low</option>
-                        </select>
-                        <button className="button">Reset</button>
-                    </div>
-                </div>
-
-                <HotelList hotels={hotels} />
-
-
-                {/* <div className="hotel-list">
+        {/* <div className="hotel-list">
                     {hotels.map(hotel => (
                         <div className="hotel-card" key={hotel.id}>
                             <div
@@ -74,9 +98,9 @@ const App = () => {
                         </div>
                     ))}
                 </div> */}
-            </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default App;
